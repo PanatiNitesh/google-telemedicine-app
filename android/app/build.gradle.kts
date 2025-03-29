4db8e6f8118dd65f165db64e43e88ed505d50128
ravindraogg <ravi20072005@gmail.com>
2025-03-29 23:30:50 +0530

    video-page

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     // ✅ Required for Flutter Local Notifications
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation ("androidx.multidex:multidex:2.0.1")
 }
 
 flutter {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,26 @@
+import org.gradle.api.tasks.Delete
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
+
+    configurations.all {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "org.webrtc" && requested.name == "google-webrtc") {
+                useVersion("1.0.32006")  // Ensures correct WebRTC version
+            }
+        }
+    }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+// Set custom build directory
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
     project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
 
